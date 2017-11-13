db3625e5bb08df24360edef09a0c8944607bfb04
Larry Zhang <lzhang124@gmail.com>
2017-11-13 15:52:08 -0500

    vgg bn

--- model/tensorflow/vgg_bn_train.py
+++ model/tensorflow/vgg_bn_train.py
@@ -1,7 +1,7 @@
 import datetime
 import numpy as np
 from keras.models import Sequential, load_model
-from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.core import Activation, BatchNormalization, Dense, Dropout, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 from keras.metrics import categorical_accuracy, top_k_categorical_accuracy
@@ -15,45 +15,75 @@ def accuracy5(y_true, y_pred):
 def VGG_16(weights_path=None):
     model = Sequential()
     model.add(ZeroPadding2D((1,1),input_shape=(224,224,3)))
-    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(Convolution2D(64, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(Convolution2D(64, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(Convolution2D(128, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(Convolution2D(128, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(Convolution2D(256, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(Convolution2D(256, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(256, (3, 3), activation='relu'))
+    model.add(Convolution2D(256, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(Convolution2D(512, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(Convolution2D(512, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(Convolution2D(512, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(Convolution2D(512, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(Convolution2D(512, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(ZeroPadding2D((1,1)))
-    model.add(Convolution2D(512, (3, 3), activation='relu'))
+    model.add(Convolution2D(512, (3, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(MaxPooling2D((2,2), strides=(2,2)))
 
     model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
+    model.add(Dense(4096))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(4096, activation='relu'))
+    model.add(Dense(4096))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(100, activation='softmax'))
 
@@ -63,7 +93,7 @@ def VGG_16(weights_path=None):
     return model
 
 if __name__ == "__main__":
-    batch_size = 64
+    batch_size = 256
     load_size = 256
     fine_size = 224
     c = 3

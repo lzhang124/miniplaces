139afbb1d0fcc36a77f18bb158107cbf6c79d970
Larry Zhang <lzhang124@gmail.com>
2017-11-16 17:40:26 -0500

    keras alexnet bn

--- model/tensorflow/alexnet_bn_train.py
+++ model/tensorflow/alexnet_bn_train.py
@@ -1,207 +1,163 @@
-import os, datetime
+import argparse
 import numpy as np
-import tensorflow as tf
-from tensorflow.contrib.layers.python.layers import batch_norm
+from keras.callbacks import ModelCheckpoint
+from keras.models import Sequential, load_model
+from keras.layers import BatchNormalization
+from keras.layers.core import Activation, Dense, Dropout, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.metrics import categorical_accuracy, top_k_categorical_accuracy
+from keras.utils.np_utils import to_categorical
 from DataLoader import *
 
-# Dataset Parameters
-batch_size = 256
-load_size = 256
-fine_size = 224
-c = 3
-data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
 
-# Training Parameters
-learning_rate = 0.001
-dropout = 0.5 # Dropout, probability to keep units
-training_iters = 50000
-step_display = 50
-step_save = 10000
-path_save = 'alexnet_bn'
-start_from = ''
+def AlexNet():
+    model = Sequential()
+    model.add(Convolution2D(96, (11, 11), strides=(4,4), input_shape=(224,224,3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((3,3), strides=(2,2), padding='same'))
 
-def batch_norm_layer(x, train_phase, scope_bn):
-    return batch_norm(x, decay=0.9, center=True, scale=True,
-    updates_collections=None,
-    is_training=train_phase,
-    reuse=None,
-    trainable=True,
-    scope=scope_bn)
+    model.add(Convolution2D(256, (5, 5), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((3,3), strides=(2,2), padding='same'))
+
+    model.add(Convolution2D(384, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(384, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(256, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((3,3), strides=(2,2), padding='same'))
+
+    model.add(Flatten())
+    model.add(Dense(4096))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation='softmax'))
+
+    return model
+
+
+def create_generator(loader, batch_size):
+    loader.reset()
+    while True:
+        yield loader.next_batch(batch_size)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', default=25, type=int)
+    parser.add_argument('-e', default=500, type=int)
+    parser.add_argument('-l', '--load', default=False, action='store_true')
+    parser.add_argument('-f', '--file', default='vgg19_bn.h5')
+    parser.add_argument('-v', '--val', default=True, action='store_false')
+    parser.add_argument('-t', '--test', default=True, action='store_false')
+    args = parser.parse_args()
     
-def alexnet(x, keep_dropout, train_phase):
-    weights = {
-        'wc1': tf.Variable(tf.random_normal([11, 11, 3, 96], stddev=np.sqrt(2./(11*11*3)))),
-        'wc2': tf.Variable(tf.random_normal([5, 5, 96, 256], stddev=np.sqrt(2./(5*5*96)))),
-        'wc3': tf.Variable(tf.random_normal([3, 3, 256, 384], stddev=np.sqrt(2./(3*3*256)))),
-        'wc4': tf.Variable(tf.random_normal([3, 3, 384, 256], stddev=np.sqrt(2./(3*3*384)))),
-        'wc5': tf.Variable(tf.random_normal([3, 3, 256, 256], stddev=np.sqrt(2./(3*3*256)))),
+    batch_size = args.b
+    epochs = args.e
+    load_size = 256
+    fine_size = 224
+    lr = 0.0001
+    data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
+    path_save = args.file
+    load = args.load
 
-        'wf6': tf.Variable(tf.random_normal([7*7*256, 4096], stddev=np.sqrt(2./(7*7*256)))),
-        'wf7': tf.Variable(tf.random_normal([4096, 4096], stddev=np.sqrt(2./4096))),
-        'wo': tf.Variable(tf.random_normal([4096, 100], stddev=np.sqrt(2./4096)))
-    }
+    opt_data_train = {
+        'data_root': '../../data/images/',
+        'data_list': '../../data/train.txt',
+        'load_size': load_size,
+        'fine_size': fine_size,
+        'data_mean': data_mean,
+        'randomize': True,
+        'num_categories': 100,
+        'labels': True
+        }
+    opt_data_val = {
+        'data_root': '../../data/images/',
+        'data_list': '../../data/val.txt',
+        'load_size': load_size,
+        'fine_size': fine_size,
+        'data_mean': data_mean,
+        'randomize': False,
+        'num_categories': 100,
+        'labels': True
+        }
+    opt_data_test = {
+        'data_root': '../../data/images/',
+        'data_list': '../../data/test.txt',
+        'load_size': load_size,
+        'fine_size': fine_size,
+        'data_mean': data_mean,
+        'randomize': False,
+        'num_categories': 100,
+        'labels': False
+        }
 
-    biases = {
-        'bo': tf.Variable(tf.ones(100))
-    }
+    loader_train = DataLoaderDisk(**opt_data_train)
+    loader_val = DataLoaderDisk(**opt_data_val)
+    loader_test = DataLoaderDisk(**opt_data_test)
 
-    # Conv + ReLU + Pool, 224->55->27
-    conv1 = tf.nn.conv2d(x, weights['wc1'], strides=[1, 4, 4, 1], padding='SAME')
-    conv1 = batch_norm_layer(conv1, train_phase, 'bn1')
-    conv1 = tf.nn.relu(conv1)
-    pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
+    assert loader_val.size() % batch_size == 0, 'Batch size must be a divisor of {}'.format(loader_val.size())
+    steps_per_epoch = loader_train.size() / batch_size
+    validation_steps = loader_val.size() / batch_size
+    test_steps = loader_test.size() / batch_size
 
-    # Conv + ReLU  + Pool, 27-> 13
-    conv2 = tf.nn.conv2d(pool1, weights['wc2'], strides=[1, 1, 1, 1], padding='SAME')
-    conv2 = batch_norm_layer(conv2, train_phase, 'bn2')
-    conv2 = tf.nn.relu(conv2)
-    pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-    # Conv + ReLU, 13-> 13
-    conv3 = tf.nn.conv2d(pool2, weights['wc3'], strides=[1, 1, 1, 1], padding='SAME')
-    conv3 = batch_norm_layer(conv3, train_phase, 'bn3')
-    conv3 = tf.nn.relu(conv3)
-
-    # Conv + ReLU, 13-> 13
-    conv4 = tf.nn.conv2d(conv3, weights['wc4'], strides=[1, 1, 1, 1], padding='SAME')
-    conv4 = batch_norm_layer(conv4, train_phase, 'bn4')
-    conv4 = tf.nn.relu(conv4)
-
-    # Conv + ReLU + Pool, 13->6
-    conv5 = tf.nn.conv2d(conv4, weights['wc5'], strides=[1, 1, 1, 1], padding='SAME')
-    conv5 = batch_norm_layer(conv5, train_phase, 'bn5')
-    conv5 = tf.nn.relu(conv5)
-    pool5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-    # FC + ReLU + Dropout
-    fc6 = tf.reshape(pool5, [-1, weights['wf6'].get_shape().as_list()[0]])
-    fc6 = tf.matmul(fc6, weights['wf6'])
-    fc6 = batch_norm_layer(fc6, train_phase, 'bn6')
-    fc6 = tf.nn.relu(fc6)
-    fc6 = tf.nn.dropout(fc6, keep_dropout)
-    
-    # FC + ReLU + Dropout
-    fc7 = tf.matmul(fc6, weights['wf7'])
-    fc7 = batch_norm_layer(fc7, train_phase, 'bn7')
-    fc7 = tf.nn.relu(fc7)
-    fc7 = tf.nn.dropout(fc7, keep_dropout)
-
-    # Output FC
-    out = tf.add(tf.matmul(fc7, weights['wo']), biases['bo'])
-    
-    return out
-
-# Construct dataloader
-opt_data_train = {
-    #'data_h5': 'miniplaces_256_train.h5',
-    'data_root': '../../data/images/',   # MODIFY PATH ACCORDINGLY
-    'data_list': '../../data/train.txt', # MODIFY PATH ACCORDINGLY
-    'load_size': load_size,
-    'fine_size': fine_size,
-    'data_mean': data_mean,
-    'randomize': True
-    }
-opt_data_val = {
-    #'data_h5': 'miniplaces_256_val.h5',
-    'data_root': '../../data/images/',   # MODIFY PATH ACCORDINGLY
-    'data_list': '../../data/val.txt',   # MODIFY PATH ACCORDINGLY
-    'load_size': load_size,
-    'fine_size': fine_size,
-    'data_mean': data_mean,
-    'randomize': False
-    }
-
-loader_train = DataLoaderDisk(**opt_data_train)
-loader_val = DataLoaderDisk(**opt_data_val)
-#loader_train = DataLoaderH5(**opt_data_train)
-#loader_val = DataLoaderH5(**opt_data_val)
-
-# tf Graph input
-x = tf.placeholder(tf.float32, [None, fine_size, fine_size, c])
-y = tf.placeholder(tf.int64, None)
-keep_dropout = tf.placeholder(tf.float32)
-train_phase = tf.placeholder(tf.bool)
-
-# Construct model
-logits = alexnet(x, keep_dropout, train_phase)
-
-# Define loss and optimizer
-loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits))
-train_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-
-# Evaluate model
-accuracy1 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(logits, y, 1), tf.float32))
-accuracy5 = tf.reduce_mean(tf.cast(tf.nn.in_top_k(logits, y, 5), tf.float32))
-
-# define initialization
-init = tf.global_variables_initializer()
-
-# define saver
-saver = tf.train.Saver()
-
-# define summary writer
-#writer = tf.train.SummaryWriter('.', graph=tf.get_default_graph())
-
-# Launch the graph
-with tf.Session() as sess:
-    # Initialization
-    if len(start_from)>1:
-        saver.restore(sess, start_from)
+    if load:
+        model = load_model(path_save)
     else:
-        sess.run(init)
-    
-    step = 0
+        model = AlexNet()
+        sgd = SGD(lr=lr, decay=1e-6, momentum=0.9, nesterov=True)
+        model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=[categorical_accuracy, top_k_categorical_accuracy])
 
-    while step < training_iters:
-        # Load a batch of training data
-        images_batch, labels_batch = loader_train.next_batch(batch_size)
-        
-        if step % step_display == 0:
-            print('[%s]:' %(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        checkpoint = ModelCheckpoint(path_save)
+        callbacks_list = [checkpoint]
+        model.fit_generator(
+            generator=create_generator(loader_train, batch_size),
+            steps_per_epoch=steps_per_epoch,
+            epochs=epochs,
+            callbacks=callbacks_list,
+            validation_data=create_generator(loader_val, batch_size),
+            validation_steps=validation_steps
+        )
+        model.save(model_file)
+        print 'Training Finished!'
 
-            # Calculate batch loss and accuracy on training set
-            l, acc1, acc5 = sess.run([loss, accuracy1, accuracy5], feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False}) 
-            print("-Iter " + str(step) + ", Training Loss= " + \
-                  "{:.6f}".format(l) + ", Accuracy Top1 = " + \
-                  "{:.4f}".format(acc1) + ", Top5 = " + \
-                  "{:.4f}".format(acc5))
+    if args.val:
+        print 'Validating...'
+        loader_val.reset()
+        l, acc1, acc5 = model.evaluate_generator(
+            generator=create_generator(loader_val, batch_size),
+            steps=validation_steps
+        )
+        print 'loss: {}, acc1: {}, acc5: {}'.format(l, acc1, acc5)
 
-            # Calculate batch loss and accuracy on validation set
-            images_batch_val, labels_batch_val = loader_val.next_batch(batch_size)    
-            l, acc1, acc5 = sess.run([loss, accuracy1, accuracy5], feed_dict={x: images_batch_val, y: labels_batch_val, keep_dropout: 1., train_phase: False}) 
-            print("-Iter " + str(step) + ", Validation Loss= " + \
-                  "{:.6f}".format(l) + ", Accuracy Top1 = " + \
-                  "{:.4f}".format(acc1) + ", Top5 = " + \
-                  "{:.4f}".format(acc5))
-        
-        # Run optimization op (backprop)
-        sess.run(train_optimizer, feed_dict={x: images_batch, y: labels_batch, keep_dropout: dropout, train_phase: True})
-        
-        step += 1
-        
-        # Save model
-        if step % step_save == 0:
-            saver.save(sess, path_save, global_step=step)
-            print("Model saved at Iter %d !" %(step))
-        
-    print("Optimization Finished!")
+    if args.test:
+        print 'Predicting...'
+        preds = model.predict_generator(
+            generator=create_generator(loader_test, batch_size),
+            steps=test_steps,
+            verbose=1
+        )
 
+        print 'Saving predictions...'
+        with open('../../data/test.txt','r') as lines:
+            filenames = [line.split(' ')[0] for line in lines]
 
-    # Evaluate on the whole validation set
-    print('Evaluation on the whole validation set...')
-    num_batch = loader_val.size()//batch_size
-    acc1_total = 0.
-    acc5_total = 0.
-    loader_val.reset()
-    for i in range(num_batch):
-        images_batch, labels_batch = loader_val.next_batch(batch_size)    
-        acc1, acc5 = sess.run([accuracy1, accuracy5], feed_dict={x: images_batch, y: labels_batch, keep_dropout: 1., train_phase: False})
-        acc1_total += acc1
-        acc5_total += acc5
-        print("Validation Accuracy Top1 = " + \
-              "{:.4f}".format(acc1) + ", Top5 = " + \
-              "{:.4f}".format(acc5))
-
-    acc1_total /= num_batch
-    acc5_total /= num_batch
-    print('Evaluation Finished! Accuracy Top1 = ' + "{:.4f}".format(acc1_total) + ", Top5 = " + "{:.4f}".format(acc5_total))
+        with open('../../evaluation/test.pred.txt','w') as file:
+            top_indices = preds.argsort()[:,-1:-6:-1]
+            for i in xrange(len(preds)):
+                top5 = ' '.join(str(j) for j in top_indices[i])
+                file.write(filenames[i] + ' ' + top5 + '\n')
